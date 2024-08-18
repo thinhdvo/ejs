d85a0c344bab77a46ea8d0aa34252baab65e4803
thinhdvo <48504790+thinhdvo@users.noreply.github.com>
2024-08-18 02:58:33 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,24 +2,16 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-
-const d = new Date("August 17, 2024");
-let day = d.getDay();
-let type = "a weekday";
-let adv = "it's time to work hard!";
-
-if (day === 0 || day === 6) {
-    type = "the weekend"
-    adv = "let have some fun!"
-};
-
 app.get("/", (req, res) => {
-    console.log(day);
-    res.render("index.ejs", {
-        dayType: type,
-        advice: adv,
-    });
+    const data = {
+        title: "EJS Tags",
+        seconds: new Date().getSeconds(),
+        items: ["apple", "banana", "cherry"],
+        htmlContent: "<strong>This is some strong text</strong>",
+    };
+    res.render("index2.ejs", data);
 });
+
 app.listen(port, () => {
-    console.log(`App is running on ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
